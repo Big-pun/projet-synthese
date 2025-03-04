dfb075f2ddffd5213ae5345d1436f38bd0ed5040
Big-pun <150970603+Big-pun@users.noreply.github.com>
2025-03-03 21:48:13 -0500

    Setup Pinia / Tailwind / Routeur / ESLint / .gitignore

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="container mx-auto">
+    <header>
+      <div>
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+        </nav>
+      </div>
+    </header>
+    <RouterView />
+  </div>
+</template>
+
+<style scoped></style>
