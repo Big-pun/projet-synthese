e1b2cf60d610544bcb3139d031454f33495e03a8
Jeanne Pietropaoli <j.pietropaoli@yahoo.fr>
2025-03-06 02:53:13 +0100

    BudgetView: Add all its components to the page

--- src/view/BudgetView.vue
+++ src/view/BudgetView.vue
@@ -1,8 +1,14 @@
 <script setup>
+import BudgetPreview from '@/components/budget/BudgetPreview.vue';
+import IncomeTable from '@/components/budget/IncomeTable.vue';
+import SpendingTable from '@/components/budget/SpendingTable.vue';
 </script>
 
 <template>
-  Budget works
+  <p class="font-bold">CONTENU PAGE BUDGET</p>
+  <BudgetPreview></BudgetPreview>
+  <IncomeTable></IncomeTable>
+  <SpendingTable></SpendingTable>
 </template>
 
 <style></style>
