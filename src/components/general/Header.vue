c80b20df631e65f68138549b05c6b9e893d3ad3b
Jeanne Pietropaoli <j.pietropaoli@yahoo.fr>
2025-03-10 18:33:50 +0100

    Put temporary nav inside Header.vue

--- src/components/general/Header.vue
+++ src/components/general/Header.vue
@@ -2,7 +2,20 @@
 </script>
 
 <template>
-  <p>Header works</p>
+  <nav class="flex gap-6">
+    <RouterLink to="/accueil" class="flex font-medium text-lg text-color-dark-gray gap-3 items-center">
+      <img class="w-14" src="../../assets/icons/home_logo.svg" alt="aller a la page accueil">
+      Accueil
+    </RouterLink>
+    <RouterLink to="/espace-client/budget" class="flex font-medium text-lg text-color-dark-gray gap-3 items-center">
+      <img class="w-14" src="../../assets/icons/budget_logo.svg" alt="aller a la page budget">
+      Budget
+    </RouterLink>
+    <RouterLink to="/espace-client/profil" class="flex font-medium text-lg text-color-dark-gray gap-3 items-center">
+      <img class="w-14" src="../../assets/icons/profil_logo.svg" alt="aller a la page profile">
+      Profil
+    </RouterLink>
+   </nav>
 </template>
 
 <style></style>
