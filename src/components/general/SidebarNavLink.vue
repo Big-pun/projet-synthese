08d2210272b8606365bbfe9b7a901cd2f7c1eaa4
Jeanne Pietropaoli <j.pietropaoli@yahoo.fr>
2025-03-11 00:37:00 +0100

    Create SidebarNavLink component

--- src/components/general/SidebarNavLink.vue
+++ src/components/general/SidebarNavLink.vue
@@ -1,8 +1,17 @@
 <script setup>
+import { RouterLink } from 'vue-router'
+
+const props = defineProps({
+  link: Object,
+  collapsed: Boolean
+})
 </script>
 
 <template>
-  <p>SidebarNavLink works</p>
+  <RouterLink :to="link.route" exact-active-class="active" class="nav__link relative bg-transparent px-4 md:px-6 py-3 flex font-medium text-md text-color-dark-gray items-center gap-4 w-full">
+    <img class="w-12" :src="link.icon" :alt="'aller a la page ' + link.name">
+    <span v-if="!collapsed" class="hidden md:inline-block pr-4 md:pr-18">{{ link.name }}</span>
+  </RouterLink>
 </template>
 
 <style></style>
