f4524fa3815d5149424c2810eb499e49235bce3e
Jeanne Pietropaoli <j.pietropaoli@yahoo.fr>
2025-03-10 18:35:41 +0100

    Add test content to BudgetView to test ClientInterface layout

--- src/view/BudgetView.vue
+++ src/view/BudgetView.vue
@@ -9,6 +9,13 @@ import SpendingTable from '@/components/budget/SpendingTable.vue';
   <BudgetPreview></BudgetPreview>
   <IncomeTable></IncomeTable>
   <SpendingTable></SpendingTable>
+  <div class="h-80 bg-light-gray w-40 m-5">Testing purposes</div>
+  <div class="h-80 bg-light-gray w-40 m-5">Testing purposes</div>
+  <div class="h-80 bg-light-gray w-40 m-5">Testing purposes</div>
+  <div class="h-80 bg-light-gray w-40 m-5">Testing purposes</div>
+  <div class="h-80 bg-light-gray w-40 m-5">Testing purposes</div>
+  <div class="h-80 bg-light-gray w-40 m-5">Testing purposes</div>
+  <div class="h-80 bg-light-gray w-40 m-5">Testing purposes</div>
 </template>
 
 <style></style>
