4b98cb8e732ff73a33359e3d58a36bb617ee357e
Jeanne Pietropaoli <j.pietropaoli@yahoo.fr>
2025-03-11 16:24:42 +0100

    Create Budget Preview section

--- src/components/budget/BudgetPreview.vue
+++ src/components/budget/BudgetPreview.vue
@@ -1,8 +1,84 @@
 <script setup>
+import { computed, ref } from 'vue';
+import balanceIconPositive from '@/assets/img/icons/balance_icon_positive.svg';
+import balanceIconNegative from '@/assets/img/icons/balance_icon_negative.svg';
+import pieIcon from '@/assets/img/icons/pie_icon.svg';
+import calendarIcon from '@/assets/img/icons/calendar_icon.svg';
+import logo from '@/assets/img/logo.svg';
+
+// Ces données viendront du backend plus tard
+const userBudget = ref({
+  incomes: [
+    { id: 1, name: 'Salaire', amount: 2000 },
+    { id: 2, name: 'Bourse', amount: 500 },
+    { id: 3, name: 'Aide familiale', amount: 200 },
+  ],
+  spendings: [
+    { id: 1, name: 'Loyer', amount: 500 },
+    { id: 2, name: 'Courses', amount: 200 },
+    { id: 3, name: 'Transport', amount: 100 },
+  ],
+});
+
+const incomesTotal = computed(() => {
+  return userBudget.value.incomes.reduce((acc, income) => acc + income.amount, 0);
+});
+
+const spendingsTotal = computed(() => {
+  return userBudget.value.spendings.reduce((acc, spending) => acc + spending.amount, 0);
+});
+
+const balance = computed(() => {
+  return incomesTotal.value - spendingsTotal.value;
+});
+
+// red theme if balance is negative, green theme if balance is positive
+const themeClasses = computed(() => {
+  return ({
+    borderColor: balance.value > 0 ? 'border-accent1' : 'border-accent2',
+    textColor: balance.value > 0 ? 'text-accent1' : 'text-accent2',
+    backgroundColor: balance.value > 0 ? 'bg-accent1' : 'bg-accent2',
+  })
+});
 </script>
 
 <template>
-  <p>BudgetPreview works</p>
+  <div class="mb-8 font-roboto max-w-screen-lg mx-auto">
+    <!-- Titre de la section -->
+    <div class="flex items-center mb-4 md:-ml-8">
+      <img :src="logo" alt="Logo" class="w-6 h-6 mr-2" />  
+      <h1 class="uppercase text-gray font-bold">Budget Mensuel</h1>
+    </div>
+ 
+    <!-- Section complète en grille -->
+    <div class="grid grid-cols-1 lg:grid-cols-2 space-y-6 lg:space-y-0">
+      <!-- Carte d'aperçu -->
+      <div :class="themeClasses.borderColor" class="bg-light-gray text-lg shadow-sm rounded-3xl border-b-8 flex flex-col items-center">
+        <h2 :class="themeClasses.backgroundColor" class="text-center font-medium w-full rounded-3xl rounded-b-none p-2 text-white text-lg">Balance du mois</h2>
+        <div class="flex flex-col sm:flex-row items-center space-x-4 space-y-4 w-max justify-center lg:justify-start p-6 md:p-mt-4">
+          <img :src="balance > 0 ? balanceIconPositive : balanceIconNegative" class="shrink-0 w-20 md:w-28" alt="Balance du budget">
+          <p :class="themeClasses.textColor"  class="font-normal text-[2.5rem] md:text-[3.5rem]">{{ `${balance > 0 ? '+' : ''}` }}{{ balance }}$</p>
+        </div>
+      </div>
+
+      <!-- Boutons -->
+      <div class="flex text-lg flex-col justify-center items-center space-y-4 md:space-y-6">
+        <button
+          class="w-full max-w-xs md:max-w-sm flex items-center bg-gray text-white py-3 px-6 rounded-md">
+          <img :src="calendarIcon" alt="Icon de calendrier" class="h-9 w-8" />
+          <span class="grow">21 jours restants</span>
+        </button>
+
+        <button
+          class="w-full max-w-xs md:max-w-sm flex items-center bg-gray text-white py-3 px-6 rounded-md">
+          <img :src="pieIcon" alt="Icon de diagramme circulaire" class="h-9 w-10" />
+          <span class="grow">22% du budget depense</span>
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style></style>
+<style scoped>
+
+</style>
