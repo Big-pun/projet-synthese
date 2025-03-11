8684b43e66799088904ddd3627632d2910143682
Jeanne Pietropaoli <j.pietropaoli@yahoo.fr>
2025-03-11 14:36:32 +0100

    Create Sidebar action and router link components

--- src/components/general/Sidebar.vue
+++ src/components/general/Sidebar.vue
@@ -6,9 +6,10 @@ import homeIcon from '../../assets/icons/home_icon.svg';
 import budgetIcon from '../../assets/icons/budget_icon.svg';
 import profileIcon from '../../assets/icons/profil_icon.svg';
 import signOutIcon from '../../assets/icons/sign_out_icon_alt.svg';
-import SidebarNavLink from './SidebarNavLink.vue';
+import SidebarRouterLink from './SidebarRouterLink.vue';
 import signInIcon from '../../assets/icons/sign_in_icon.svg';
 import signUpIcon from '../../assets/icons/sign_up_icon.svg';
+import SidebarActionNavLink from './SidebarActionNavLink.vue';
 
 const props = defineProps({
   userConnected: Boolean
@@ -16,75 +17,78 @@ const props = defineProps({
 
 const collapsed = ref(true);
 
-function collapseSidebar() {
+function toggleSidebar() {
   collapsed.value = !collapsed.value;
 }
-
-const userConnectedNavLinks = [
-  {
-    name: 'Accueil',
-    icon: homeIcon,
-    route: '/accueil'
-  },
-  {
-    name: 'Budget',
-    icon: budgetIcon,
-    route: '/budget'
-  },
-  {
-    name: 'Profil',
-    icon: profileIcon,
-    route: '/profil'
-  }
-]
 
 const homeLink = {
   name: 'Accueil',
   icon: homeIcon,
   route: '/accueil'
 }
+
+const budgetLink = {
+  name: 'Budget',
+  icon: budgetIcon,
+  route: '/budget'
+}
+
+const profileLink = {
+  name: 'Profil',
+  icon: profileIcon,
+  route: '/profil'
+}
+
+const userConnectedNavLinks = [homeLink, budgetLink, profileLink];
+
+const openSubscriptionModal = () => {
+  // **** logic to open subscription modal to add
+  console.log('open subscription modal');
+}
+
+const openConnexionModal = () => {
+  // **** logic to open connexion modal to add
+  console.log('open connexion modal');
+}
+
+const signOutUser = () => {
+  // **** logic to sign out user to add
+  console.log('sign out user');
+}
+
 </script>
 
 <template>
-  <div class="sidebar relative py-5 text-dark-gray bg-black h-full flex flex-col justify-center">
+  <div class="sidebar sticky w-max top-0 min-h-[100vh] h-full py-5 text-dark-gray bg-black flex flex-col justify-center">
     <!-- sidebar is expandable only on md screen and above -->
      <!-- collapse / expand sidebar button -->
-    <div @click="collapseSidebar" :class="collapsed ? 'justify-center': 'start w-max'" class="hidden md:flex nav__link relative bg-transparent px-6 py-3 font-medium text-md text-color-dark-gray items-center gap-4 cursor-pointer">
-      <img class="h-4" :src="collapsed ? menuIcon : closeIcon" :alt="collapsed ? 'Agrandir le menu' : 'Reduire le menu'"> <!-- SIGN OUT LOGIC TO ADD -->
+    <div @click="toggleSidebar" :class="collapsed ? 'justify-center': 'start w-max'" class="hidden md:flex nav__link relative bg-transparent px-6 py-3 font-medium text-md text-color-dark-gray items-center gap-4 cursor-pointer">
+      <img class="h-4" :src="collapsed ? menuIcon : closeIcon" :alt="collapsed ? 'Agrandir le menu' : 'Reduire le menu'">
     </div>
 
     <!-- navlinks -->
     <nav class="nav flex flex-col min-h-full relative grow justify-center w-full min-w-18">
-      <!-- if user is connected -->
+      <!-- if user is connected: Home | Budget | Profile -->
       <ul v-if="userConnected">
-        <li v-for="link in userConnectedNavLinks" :key="link.route">
-          <SidebarNavLink :link="link" :collapsed="collapsed"></SidebarNavLink>
+        <li v-for="link in userConnectedNavLinks" :key="link.name">
+          <SidebarRouterLink :link="link" :collapsed="collapsed"></SidebarRouterLink>
         </li>
       </ul>
 
-      <!-- if user is not connected -->
+      <!-- if user is not connected: Home | Sign Up | Sign In -->
       <ul v-else>
-        <SidebarNavLink :link="homeLink" :collapsed="collapsed"></SidebarNavLink>
+        <SidebarRouterLink :link="homeLink" :collapsed="collapsed"></SidebarRouterLink>
 
-        <!-- ***** sign up button - @click and its logic to open up Subscription Modal will need to be added -->
-        <div class="nav__link relative bg-transparent px-4 md:px-6 py-3 flex font-medium text-md text-color-dark-gray items-center gap-4 w-full cursor-pointer">
-          <img class="w-12" :src="signUpIcon" alt="S'inscrire">
-          <span v-if="!collapsed" class="hidden md:inline-block pr-4 md:pr-18">S'inscrire</span>
-        </div>
+        <!-- ***** sign up button - logic inside openSubscriptionModal function needs to be added -->
+        <SidebarActionNavLink :iconSrc="signUpIcon" buttonText="S'inscrire" :collapsed="collapsed" @handleClick="openSubscriptionModal"></SidebarActionNavLink>
 
-        <!-- ***** sign in button - @click and its logic to open up Connexion Modal will need to be added -->
-        <div class="nav__link relative bg-transparent px-4 md:px-6 py-3 flex font-medium text-md text-color-dark-gray items-center gap-4 w-full cursor-pointer">
-          <img class="w-12" :src="signInIcon" alt="Se connecter">
-          <span v-if="!collapsed" class="hidden md:inline-block pr-4 md:pr-18">Se connecter</span>
-        </div>
+        <!-- ***** sign in button - logic inside openConnexionModal function needs to be added -->
+        <SidebarActionNavLink :iconSrc="signInIcon" buttonText="Se connecter" :collapsed="collapsed" @handleClick="openConnexionModal"></SidebarActionNavLink>
       </ul>
     </nav>
 
-    <!-- sign out button -->
-    <div class="nav__link relative bg-transparent px-2 md:px-6 py-3 flex font-medium text-md text-color-dark-gray items-center gap-4 cursor-pointer">
-        <img class="w-12" :src="signOutIcon" alt="se deconnecter"> <!-- SIGN OUT LOGIC TO ADD -->
-        <span v-if="!collapsed" class="hidden md:inline-block pr-4 md:pr-18">Deconnexion</span>
-    </div>
+     <!-- ***** sign out button - logic inside signOutUser function needs to be added -->
+    <SidebarActionNavLink :iconSrc="signOutIcon" buttonText="Deconnexion" :collapsed="collapsed" @handleClick="signOutUser"></SidebarActionNavLink>
   </div>
 </template>
 
---
+++ src/components/general/SidebarActionNavLink.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+
+const props = defineProps({
+  iconSrc: String,
+  buttonText: String,
+  collapsed: Boolean
+})
+
+const emit = defineEmits(["handleClick"]);
+
+const handleClick = () => {
+  emit("handleClick");
+};
+
+</script>
+
+<template>
+    <div @click="handleClick" class="nav__link relative bg-transparent px-4 md:px-6 py-3 flex font-medium text-md text-color-dark-gray items-center gap-4 w-full cursor-pointer">
+        <img class="w-12" :src="iconSrc" :alt="buttonText">
+        <span v-if="!collapsed" class="hidden md:inline-block pr-4 md:pr-18 min-w-max">{{ buttonText }}</span>
+    </div>
+</template>
+
+<style>
+
+.nav__link:not(.active):hover {
+  background-color: var(--color-accent1);
+  transition: all 200ms ease;
+}
+
+</style>
