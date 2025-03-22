2eee35086364a6d9cd05c5c3af2f1683c0d2e815
Jeanne Pietropaoli <j.pietropaoli@yahoo.fr>
2025-03-22 10:11:50 -0400

    Create SpendingTable with BaseBudgetTable as a base

--- src/components/budget/SpendingTable.vue
+++ src/components/budget/SpendingTable.vue
@@ -1,8 +1,26 @@
 <script setup>
+import BaseBudgetTable from './BaseBudgetTable.vue';
+
+const props = defineProps({
+  spendings: Array
+});
+
+const headers = [
+  { label: 'Description', key: 'name' },
+  { label: 'Catégorie', key: 'category' },
+  { label: '$/mois', key: 'amount' },
+  { label: 'Recurrent', key: 'recurrent' },
+  { label: 'Actions', key: 'actions' }
+];
 </script>
 
 <template>
-  <p>SpendingTable works</p>
+  <BaseBudgetTable 
+    name="Dépenses" 
+    :headers="headers" 
+    :items="spendings" 
+    @toggleReccurence="$emit('toggleReccurence', $event)"
+    @openModal="$emit('openModal', $event)"
+    @openForm="$emit('openForm')"
+  />
 </template>
-
-<style></style>
